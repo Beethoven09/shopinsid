eb936a9c5fada323a848a47f1f93327ed1e5275b
Drakariboo <108684103+Drakariboo@users.noreply.github.com>
2023-05-16 15:33:39 +0200

    test du premier component

--- Front_end/shopinsid/src/app/app.module.ts
+++ Front_end/shopinsid/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewFirstComponentComponent } from './new-first-component/new-first-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewFirstComponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ Front_end/shopinsid/src/app/new-first-component/new-first-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-first-component',
+  templateUrl: './new-first-component.component.html',
+  styleUrls: ['./new-first-component.component.scss']
+})
+export class NewFirstComponentComponent {
+
+}
