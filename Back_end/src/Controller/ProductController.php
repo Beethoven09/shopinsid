8e48d8d0e0dd82c5457c272acbb512d1bfe1ded3
Chakib MOUSSAOUI <Chakib_moussaoui@outlook.fr>
2023-05-19 14:58:09 +0200

    Created a JSON file to store the products and implemented methods to read from and write to the file.

--- Back_end/src/Controller/ProductController.php
+++ Back_end/src/Controller/ProductController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,94 +9,38 @@ use Symfony\Component\HttpFoundation\Request;
 
 class ProductController extends AbstractController
 {
-    private $products = [
-        ['id'=>1,'name'=>'Produit 1','price' => 8.99],
-        ['id'=>2,'name'=>'Produit 2','price' => 5.99]
-    ];
+    private $productsFile = __DIR__ . "/products.json";
+    private function getProducts(): array
+    {
+        $fileContent = file_get_contents($this->productsFile);
+        return json_decode($fileContent, true) ?: [];
+    }
+
+    private function saveProducts(array $products): void
+    {
+        $fileContent = json_encode($products);
+        file_put_contents($this->productsFile, $fileContent);
+    }
 
     /**
      * @Route("/products", methods="GET")
      */
     public function getAllProducts(): Response
     {
-       //Get all products
-       return new JsonResponse($this->products);
+        $products = $this->getProducts();
+        return new JsonResponse($products);
     }
 
     /** 
-     * @Route ("/Products/{id}", methods="GET") 
+     * @Route ("/products/{id}", methods="GET") 
      */
-    public function getProduct($id) : Response 
+    public function getProduct($id): Response
     {
-        $SelectedProduct = null;
-        foreach ($this->products as $product) {
-            if ($product['id'] == $id) {
-                $SelectedProduct = $product;
-                break;
-            } 
-        }
-
-        if ($SelectedProduct == null) {
-            return new JsonResponse(['error'=> 'Produit non existant !'],404);
-        }else {
-            return new JsonResponse($SelectedProduct);
-        }
-    }
-
-    /**
-     * @Route("/products",methods="POST") : Response 
-     */
-    public function createNewProduct(Request $request): JsonResponse
-    {
-        $data = json_decode($request->getContent(),true);
-
-        if(!isset($data['id']) || !isset($data['name']) || !isset($data['price'])){
-            return new JsonResponse(['error'=> 'Format incorrect']);
-        }else {
-            $newProduct = [
-                'id' => $data['id'],
-                'name' => $data['name'],
-                'price' => $data['price']
-            ];
-    
-            // Add the new product to the products array
-            $this->products[] = $newProduct;
-    
-            return new JsonResponse($newProduct, 201);
-        }
-    }
-    /**
-     * @Route("/products",methods="DELETE") : Response 
-     */
-    public function deleteProduct($id): JsonResponse
-    {
-        $SelectedProduct = null;
-        foreach ($this->products as $product) {
-            if ($product['id'] == $id) {
-                $SelectedProduct = $product;
-                break;
-            } 
-        }
-
-        if ($SelectedProduct == null) {
-            return new JsonResponse(['error'=> 'Produit non existant !'],404);
-        }else {
-            unset($this->products[$id]);
-            return new JsonResponse(['Status'=>'Produit supprimé avec succès']);
-        }
-    }
-    
-    /**
-     * @Route("/products/{id}", methods="PUT")
-     */
-    public function alterProduct($id, Request $request): JsonResponse
-    {
-        $data = json_decode($request->getContent(), true);
-
+        $products = $this->getProducts();
         $selectedProduct = null;
-        foreach ($this->products as &$product) {
+        foreach ($products as $product) {
             if ($product['id'] == $id) {
-                $selectedProduct = &$product;
+                $selectedProduct = $product;
                 break;
             }
         }
@@ -105,15 +48,86 @@ class ProductController extends AbstractController
         if ($selectedProduct == null) {
             return new JsonResponse(['error' => 'Produit non existant !'], 404);
         } else {
+            return new JsonResponse($selectedProduct);
+        }
+    }
+
+    /**
+     * @Route("/products", methods="POST")
+     */
+    public function createNewProduct(Request $request): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        if (!isset($data['id']) || !isset($data['name']) || !isset($data['price'])) {
+            return new JsonResponse(['error' => 'Format incorrect']);
+        } else {
+            $newProduct = [
+                'id' => $data['id'],
+                'name' => $data['name'],
+                'price' => $data['price']
+            ];
+
+            $products = $this->getProducts();
+            $products[] = $newProduct;
+            $this->saveProducts($products);
+
+            return new JsonResponse($newProduct, 201);
+        }
+    }
+
+    /**
+     * @Route("/products/{id}", methods="DELETE")
+     */
+    public function deleteProduct($id): JsonResponse
+    {
+        $products = $this->getProducts();
+        $selectedProductIndex = null;
+        foreach ($products as $index => $product) {
+            if ($product['id'] == $id) {
+                $selectedProductIndex = $index;
+                break;
+            }
+        }
+
+        if ($selectedProductIndex === null) {
+            return new JsonResponse(['error' => 'Produit non existant !'], 404);
+        } else {
+            unset($products[$selectedProductIndex]);
+            $this->saveProducts(array_values($products));
+            return new JsonResponse(['Status' => 'Produit supprimé avec succès']);
+        }
+    }
+
+    /**
+     * @Route("/products/{id}", methods="PUT")
+     */
+    public function alterProduct($id, Request $request): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $products = $this->getProducts();
+        $selectedProductIndex = null;
+        foreach ($products as $index => &$product) {
+            if ($product['id'] == $id) {
+                $selectedProductIndex = $index;
+                break;
+            }
+        }
+
+        if ($selectedProductIndex === null) {
+            return new JsonResponse(['error' => 'Produit non existant !'], 404);
+        } else {
             if (isset($data['name'])) {
-                $selectedProduct['name'] = $data['name'];
+                $products[$selectedProductIndex]['name'] = $data['name'];
             }
             if (isset($data['price'])) {
-                $selectedProduct['price'] = $data['price'];
+                $products[$selectedProductIndex]['price'] = $data['price'];
             }
+
+            $this->saveProducts($products);
 
             return new JsonResponse(['status' => 'Produit modifié avec succès']);
         }
     }
-
 }
