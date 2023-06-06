9c3f662a3a49b3b6038afa2c5c5d140f6d63e9ff
redarmz <reda.ramzi0104@hotmail.com>
2023-06-06 15:33:36 +0200

    commit r

--- Front_end/shopinsid/src/app/services/auth.service.ts
+++ Front_end/shopinsid/src/app/services/auth.service.ts
@@ -7,8 +7,8 @@ import { SHA256 } from 'crypto-js';
   providedIn: 'root'
 })
 export class AuthService {
-  private connexionUrl = 'https://127.0.0.1:8000/login';
-  private InscriptionUrl = 'https://127.0.0.1:8000/register';
+  private connexionUrl = 'https://127.0.0.1:8007/login';
+  private InscriptionUrl = 'https://127.0.0.1:8007/register';
 
 
   constructor(private http: HttpClient) { }
